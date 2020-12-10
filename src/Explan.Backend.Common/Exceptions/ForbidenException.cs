020db3e7a48b7c96eae3df56976b83fc669d2868
Калашников Александр <aleximuson@gmail.com>
2020-12-10 20:35:07 +0400

    #1 Add Domain exceptions

---
+++ src/Explan.Backend.Common/Exceptions/ForbidenException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Explan.Backend.Common.Exceptions
+{
+	public class ForbidenException : DomainException
+	{
+		public ForbidenException()
+		{
+		}
+
+		public ForbidenException(string message) : base(message)
+		{
+		}
+
+		public ForbidenException(string message, Exception innerException) : base(message, innerException)
+		{
+		}
+
+		protected ForbidenException(SerializationInfo info, StreamingContext context) : base(info, context)
+		{
+		}
+	}
+}
